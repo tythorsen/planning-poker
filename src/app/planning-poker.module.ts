8a62eb60e2b4ea5dc6e14f3b69cfac7c9d2b3868
tythorsen <ty.thorsen@drip.com>
2018-09-21 14:55:45 -0500

    add routing, home component

--- src/app/planning-poker.module.ts
+++ src/app/planning-poker.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
 
+import { PlanningPokerRoutingModule } from './planning-poker-routing.module';
+
+import { HomeComponent } from './home/home.component';
 import { PlanningPokerComponent } from './planning-poker.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
     PlanningPokerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlanningPokerRoutingModule
   ],
   providers: [],
   bootstrap: [PlanningPokerComponent]
